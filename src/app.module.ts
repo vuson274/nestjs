2897a3aa0f19e3724db399ca0aa45fc5e737a6d6
sonvt <vuthanhson041995@gmail.com>
2025-02-13 21:36:48 +0700

    get role

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,9 +15,9 @@ import * as process from 'node:process';
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
-      type: process.env.DB_DRIVER || 'mysql',
+      type: process.env.DB_DRIVER as  'mysql' || 'mysql',
       host: process.env.DB_HOST,
-      port: 3306,
+      port: process.env.DB_PORT,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
--- src/entities/User.ts
+++ src/entities/User.ts
@@ -14,7 +14,7 @@ export class User {
   @Column()
   password: string;
 
-  @Column()
+  @Column({default:null})
   refresh_token?: string;
 
   @Column()
