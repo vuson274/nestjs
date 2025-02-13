5f49af5517a53fe652c54b41ff72d14104628a81
vuson274 <81661638+vuson274@users.noreply.github.com>
2025-02-13 14:21:32 +0700

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,6 @@ import * as process from 'node:process';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log(process.env.JWT_SECRET);
     return 'Hello World!';
   }
 }
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,8 +1,10 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, Query } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from '../../entities/User';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
+import { UserDto } from '../../dto/user.dto';
+import { UserMapper } from '../../mapper/UserMapper';
 
 @Injectable()
 export class UsersService {
@@ -12,8 +14,8 @@ export class UsersService {
   ) {}
   async createUser(userData: Partial<User>): Promise<User> {
     const user = this.userRepository.create(userData);
-    user.createdAt = new Date();
-    user.updatedAt = new Date();
+    user.created_at = new Date();
+    user.updated_at = new Date();
     if (!userData.password) {
       throw new HttpException('Passwords do not match', HttpStatus.NOT_FOUND);
     }
@@ -45,5 +47,35 @@ export class UsersService {
     }
     user.refresh_token = hashedRefreshToken;
     return this.userRepository.save(user);
+  }
+
+  async findAll(): Promise<User[]> {
+    return await this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User | null> {
+    return await this.userRepository.findOneBy({ id });
+  }
+
+  async update(id: number, data: Partial<User>): Promise<User | null> {
+    data.updated_at = new Date();
+    if (data.password) {
+      const hadPassword = bcrypt.hashSync(data.password, 10);
+      data.password = hadPassword;
+    }
+    await this.userRepository.update(id, data);
+    return this.userRepository.findOneBy({ id });
+  }
+
+  async delete(id: number): Promise<User | null> {
+    const product = await this.userRepository.findOneBy({ id });
+    await this.userRepository.delete(id);
+    return product;
+  }
+
+  async findByRole(role: string): Promise<UserDto[]> {
+    const users = await this.userRepository.find({ where: { role: role } });
+    const userDtos = users.map((item) => UserMapper.toDtos(item));
+    return userDtos;
   }
 }
